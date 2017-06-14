5d4366e13fb4f440e142000ba3ec5fe9f60fc00e
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-14 10:53:56 -0500

    cleanup config, drop rules that match our base config eslint:recommended, use level names instead of numbers.

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
-module.exports = {
-	"extends": "eslint:recommended",
 
-	"parserOptions": {
-		"ecmaVersion": 2017,
-		"sourceType": "module",
-		"ecmaFeatures": {
-			"experimentalObjectRestSpread": true,
-			"impliedStrict": true,
-			"globalReturn": false
+const DEV = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+	extends: 'eslint:recommended',
+
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module',
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			impliedStrict: true,
+			globalReturn: false
 		}
 	},
 
-	"env": {
-		"es6": true,
-		"node": true,
-		"browser": true,
-		"jest": true
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+		jest: true
 	},
 	
-	"plugins": [
-		"import"
+	plugins: [
+		'import'
 	],
 
-	"rules": {
-		"camelcase": [2, {"properties": "always"}],
-		"consistent-return": 0,
-		"curly": [2, "all"],
-		"eqeqeq": [2, "allow-null"],
-		"indent": [2, "tab"],
-		"guard-for-in": 2,
-		"new-cap": 0,
-		"no-bitwise": 2,
-		"no-caller": 2,
-		"no-console": 2,
-		"no-extend-native": 2,
-		"no-multiple-empty-lines": [1, {"max": 3, "maxBOF": 3, "maxEOF": 1}],
-		"no-new": 2,
-		"no-shadow": [1, {"builtinGlobals": false, "hoist": "never"}],
-		"no-throw-literal": 2,
-		"no-underscore-dangle": 1,
-		"no-use-before-define": [2, "nofunc"],
-		"no-var": 2,
-		"quotes": [1, "single"],
-		"radix": 2,
-		"semi": 2,
-		"space-before-blocks": [1, "always"],
-		"space-before-function-paren": [1, {"anonymous": "always", "named": "always"}],
-		"space-infix-ops": [2, {"int32Hint": true}],
-		"space-unary-ops": [1, { "words": true, "nonwords": false }],
-		"strict": [2, "never"],
-		"wrap-iife": [2, "outside"],
-		"valid-jsdoc": 1,
+	rules: {
+		'camelcase': ['error', {'properties': 'always'}],
+		'curly': ['error', 'multi', 'consistent'],
+		'eqeqeq': ['error', 'allow-null'],
+		'guard-for-in': 'error',
+		'indent': ['error', 'tab'],
+		'no-caller': 'error',
+		'no-console': DEV ? 'warn' : 'error',
+		'no-debugger': DEV ? 'warn' : 'error',
+		'no-multiple-empty-lines': ['warn', {'max': 3, 'maxBOF': 3, 'maxEOF': 1}],
+		'no-new': 'error',
+		'no-shadow': ['warn', {'builtinGlobals': false, 'hoist': 'never', 'allow': ['done']}],
+		'no-throw-literal': 'error',
+		'no-use-before-define': ['error', 'nofunc'],
+		'no-var': 'error',
+		'quotes': ['warn', 'single'],
+		'radix': 'error',
+		'semi': 'error',
+		'space-before-blocks': ['warn', 'always'],
+		'space-before-function-paren': ['warn', {'anonymous': 'always', 'named': 'always'}],
+		'space-infix-ops': ['error', {'int32Hint': true}],
+		'space-unary-ops': ['warn', { 'words': true, 'nonwords': false }],
+		'strict': ['error', 'never'],
+		'valid-jsdoc': 'warn',
+		'wrap-iife': ['error', 'any'],
 		
-		"import/no-duplicates": 1,
-		"import/no-extraneous-dependencies": [2, {"devDependencies": ["**/test/*.js", "**/*.spec.js"]}],
-		"import/no-unresolved": [2, {"commonjs": true}],
-		"import/order": [1, {"newlines-between": "always", "groups": ["builtin", "external", "internal", "parent", "sibling", "index"]}],
+		'import/no-duplicates': 'warn',
+		'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/test/*.js', '**/*.spec.js']}],
+		'import/no-unresolved': ['error', {'commonjs': true}],
+		'import/order': ['warn', {'newlines-between': 'always', 'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']}],
 		'import/first': 'error',
 		'import/no-amd': 'error',
 		'import/no-commonjs': 'error',
